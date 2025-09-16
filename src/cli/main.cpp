816e41681895ac6e0a907af1a6452b3f017f947b
gevikhn <gevikhn@gmail.com>
2025-09-16 18:49:06 +0800

    更新 CMakeLists.txt 以添加更多编译选项，修改 main.cpp 以支持 UTF-8 控制台编码

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -3,9 +3,18 @@
 #include "memory/core/logger.h"
 #include "memory/core/config.h"
 #include <iostream>
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 int main(int argc, char* argv[]) {
     try {
+#ifdef _WIN32
+        // 设置控制台为UTF-8编码
+        SetConsoleOutputCP(CP_UTF8);
+        SetConsoleCP(CP_UTF8);
+        // system("chcp 65001");
+#endif
         // Initialize logging system
         auto& logger = memory::core::Logger::getInstance();
         logger.setLevel(memory::core::LogLevel::INFO);
